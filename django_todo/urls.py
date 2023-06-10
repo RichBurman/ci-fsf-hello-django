adc29039952fc8893b5de8866765b225728fb87e
RichBurman <>
2023-06-10 12:01:19 +0000

    Update URL

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     path('', views.get_todo_list, name='get_todo_list'),
     path('add', views.add_item, name='add'),
     path('edit/<item_id>', views.edit_item, name='edit'),
-    path('toogle/<item_id>', views.toogle_item, name='toogle'),
+    path('toggle/<item_id>', views.toggle_item, name='toggle'),
     path('delete/<item_id>', views.delete_item, name='delete'),
-
 ]
