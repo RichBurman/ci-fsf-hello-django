84ef197de5986827ab990cfdcfbce161fb8669fc
RichBurman <>
2023-06-10 11:27:43 +0000

    Amend settings

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 if os.path.isfile('env.py'):
     import env
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '---sercetkeyvalue---')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
